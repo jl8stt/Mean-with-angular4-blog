e6e35e6351d4b456d2e1c087e395e8893324d1ba
Emma Liu <Emma@Lius-MacBook-Pro.local>
2017-11-13 20:25:46 -0500

    server-setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const config = require('./config/database');
+const path = require('path');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri);
+
+app.use(express.static(__dirname + '/client/dist/'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/dist/index.html'))
+})
+
+app.listen(8080, () => {
+  console.log(config.secret)
+  console.log('listening on port 8080')
+})
